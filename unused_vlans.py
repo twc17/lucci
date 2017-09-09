3a868732342db9dce03a7ff4182fec1fc55ec401
Troy <twc17@pitt.edu>
2017-09-09 19:18:41 -0400

    Slowly adding things. Currently typecast issues...

--- unused_vlans.py
+++ unused_vlans.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 # Author: Troy W. Caro <twc17@pitt.edu>
 # Date: Sep 8, 2017 
@@ -8,36 +8,38 @@
 #   This script uses the output file generated from vlan_usage_via_arp_entries.pl
 #   It will then output all of the VLANs with 2 or less entries in the ARP table
 #
-# Useage:
-#   python3 ununsed_vlans.py <FILE_OF_ARP_ENTRIES>.txt
+# Usage:
+#   python3 ununsed_vlans.py FILE_OF_ARP_ENTRIES.log
+#
+# Note:
+#   Not using any of the mail stuff yet
 
 # Imports
 import os
 import sys
-import smtplib
+import argparse
+# import smtplib
 
-arp_file = open(input_arp_file, 'r')
+parser = argparse.ArgumentParser(description='Extract unused VLANs from ARP entry log file')
+parser.add_argument('input_arp_file', metavar='FILE', help="Log file containing number of ARP entries per VLAN") 
+args = parser.parse_args()
+
+arp_file = open(args.input_arp_file, 'r')
+
 
 for line in arp_file:
     line = line.split(',')
-    if line[-1] <= 2:                                               # This will get the last item of the list
+    if int(line[-1]) <= 2:                                               # This will get the last item of the list, also need to strip newline char
+        print("YES")
         output = open('unused_vlans.txt', 'w')
-        output.write(line + '\n')
+        output.write(",".join(line + '\n'))
         output.close()
 
 arp_file.close()                                                    # Don't forget to close the file after reading!
 
 # Time to build the email
-msg = MIMEMultipart()
-msg['Subject'] = "Unused VLANs"
-msg['From'] = "nocjob@tsunami.ns.pitt.edu"
-msg['To'] = "pittnet-techs-l@list.pitt.edu"
+# msg = MIMEMultipart()
+# msg['Subject'] = "Unused VLANs"
+# msg['From'] = "nocjob@tsunami.ns.pitt.edu"
+# msg['To'] = "pittnet-techs-l@list.pitt.edu"
 
-# For line in log file
-#   split by ','
-#   *whatever field number of entries is
-#   if number of entries is <=2:
-#       write line to output file
-# send mail to pitt-techs list
-
-# Thats basically it
