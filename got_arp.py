f58fb13750ba4742a9643821ae22b5215fc84193
Troy <twc17@pitt.edu>
2017-09-14 07:41:35 -0400

    Starting new program

---
+++ got_arp.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+# Author: Troy W. Caro <twc17@pitt.edu>
+# Date: Sep 14, 2017
+# Version: 1.0.0
+#
+# Purpose:
+#   This script will log in to a list of routers and grab the ARP table
+#
+# Dependencies:
+#   python 2.6+
+#   netmiko
+#
+# Usage:
+#   python got_arp.py ROUTER_LIST 
+
+# Imports
+import os
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(description='Gather number of ARP entries for each VLAN')
+parser.add_argument('input_router_file', metavar='FILE', help="Text file conntaining the FQDN router list")
